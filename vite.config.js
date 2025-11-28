d15556630195771bc81bc588dfc526c2b66b4585
Atef fawzy <ateffawzy897@gmail.com>
2025-11-28 14:36:51 +0200

    Refactor Vite configuration to conditionally set base path based on Vercel environment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-  // base: "/mental-health/",
-  plugins: [react(), tailwindcss()],
+
+export default defineConfig(({ command, mode }) => {
+  const isVercel = process.env.VERCEL;
+  return {
+    plugins: [react(), tailwindcss()],
+    base: isVercel ? "/" : "/mental-health/",
+  };
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // base: "/mental-health/",
+//   plugins: [react(), tailwindcss()],
+// });
